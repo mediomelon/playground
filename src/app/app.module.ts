e76276c14098da89004f2f3a319c8b31b99bc0d5
Christian Alan Lundberg <christianalanlundberg@gmail.com>
2019-04-15 12:02:20 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatToolbarModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ src/app/app.routing.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+
+// const routes: Routes = [{
+//     path: '',
+
+// }];
